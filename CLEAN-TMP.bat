e887c4ddc0e2d2850846e6becf731094cdfea67b
fat <neelix@gmx.org>
2017-03-02 19:57:46 +0100

    Add files via upload

---
+++ CLEAN-TMP.bat
@@ -0,0 +1,27 @@
+@del *.aux
+@del *.lof
+@del *.lot
+@del *.out
+@del *.toc
+@del *.bbl
+@del *.blg
+@del *.brf
+@del *.idx
+@del *.glo
+@del *.glg
+@del *.ilg
+@del *.ind
+@del *.lol
+@del *.gls
+@del *.nls
+@del *.nlo
+@del *.acn
+@del *.alg
+@del *.slg
+@del *.syg
+@del *.syi
+@del *.ist
+@del *.acr
+@del *.bcf
+
+
